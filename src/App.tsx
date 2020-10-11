13ed4056d195cd0ded967ae41650891fb7c5f06c
YingHuaLiu <1042413535@qq.com>
2020-10-11 22:00:09 +0800

    test

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+import Nav from './components/Nav';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main=styled.div`
+  flex-grow: 1;
+`
+
+export default function App() {
+  return (
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            {/*默认路由*/}
+            <Redirect exact from='/' to='/money'/>
+            <Route to='*'>
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+
+        <Nav/>
+      </Wrapper>
+    </Router>
+  );
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch() {
+  return <div>你输错了</div>
+}
